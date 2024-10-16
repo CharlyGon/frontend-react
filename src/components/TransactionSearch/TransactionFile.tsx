c3611d1f886f3a4b163717b222102b61e4f09fd6
Gonzalo <Fernandez-gonza@hotmail.com>
2024-10-16 17:36:07 -0300

    Enhance FileInfo component: Improve UI for better readability and professional appearance

--- src/components/TransactionSearch/TransactionFile.tsx
+++ src/components/TransactionSearch/TransactionFile.tsx
@@ -1,6 +1,7 @@
 import React from "react";
-import styles from "./styles/FileTransaction.module.css";
 import { FileInfoProps } from "../../interfaces/interfaces";
+
+import styles from "./styles/FileTransaction.module.css";
 
 /**
  * Component to display basic information about the selected file.
@@ -8,22 +9,31 @@ import { FileInfoProps } from "../../interfaces/interfaces";
  * @param {FileInfoProps} props - File information containing name, day, fund, and loading/error states.
  * @returns {JSX.Element} File info component.
  */
-export const FileInfo: React.FC<FileInfoProps> = ({ fileDetails, loading }: FileInfoProps): JSX.Element => {
+export const FileInfo: React.FC<FileInfoProps> = ({
+    fileDetails,
+    loading,
+}: FileInfoProps): JSX.Element => {
     if (loading) {
-        return <p className={styles.loadingMessage}>Cargando detalles del archivo...</p>;
+        return <p className={styles.loadingMessage}>
+            Cargando detalles del archivo...
+        </p>;
     }
 
-      if (!fileDetails) {
-        return <p className={styles.noDetailsMessage}>No hay detalles disponibles para el archivo seleccionado.</p>;
+    if (!fileDetails) {
+        return <p className={styles.noDetailsMessage}>
+            No hay detalles disponibles para el archivo seleccionado.
+        </p>;
     }
 
     return (
         <div className={styles.fileInfoContainer}>
-            <h4>Detalles del Archivo</h4>
-            <p><strong>Nombre:</strong> {fileDetails.nombre}</p>
-            <p><strong>Día:</strong> {new Date(fileDetails.fecha).toLocaleDateString()}</p>
-            <p><strong>Fondo:</strong> {fileDetails.identificadorFondo}</p>
-            <p><strong>Orden:</strong> {fileDetails.orden}</p>
+            <h3 className={styles.fileInfoTitle}>Detalles del Archivo</h3>
+            <div className={styles.fileInfoDetails}>
+                <p><span className={styles.infoLabel}>Nombre:</span> {fileDetails.nombre}</p>
+                <p><span className={styles.infoLabel}>Día:</span> {new Date(fileDetails.fecha).toLocaleDateString()}</p>
+                <p><span className={styles.infoLabel}>Fondo:</span> {fileDetails.identificadorFondo}</p>
+                <p><span className={styles.infoLabel}>Orden:</span> {fileDetails.orden}</p>
+            </div>
         </div>
     );
 };
